830fb56271fe7f8403098e0fc2a8aef6bfb4d805
PandaHawk <mchlbng@gmail.com>
2025-07-12 20:02:33 +0200

    create unit test for the stateless service

--- internal/user/handler_test.go
+++ internal/user/handler_test.go
@@ -3,16 +3,16 @@ package user
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func setupTestRouter() *gin.Engine {
+func setupTestRouter(service UserService) *gin.Engine {
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
-	service := NewSimpleService()
 	handler := NewHandler(service)
 	userGroup := router.Group("/users")
 	handler.RegisterRoutes(userGroup)
@@ -20,8 +20,14 @@ func setupTestRouter() *gin.Engine {
 }
 
 func Test_getAllUsers(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockService := NewMockUserService(ctrl)
+	mockService.EXPECT().
+		GetAllUsers().
+		Return("Get all users")
 
-	router := setupTestRouter()
+	router := setupTestRouter(mockService)
 
 	req, _ := http.NewRequest(http.MethodGet, "/users", nil)
 	w := httptest.NewRecorder()
@@ -29,11 +35,17 @@ func Test_getAllUsers(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.JSONEq(t, `{"message": "get all users"}`, w.Body.String())
+	assert.JSONEq(t, `{"message": "Get all users"}`, w.Body.String())
 }
 
 func Test_getUserById(t *testing.T) {
-	router := setupTestRouter()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockService := NewMockUserService(ctrl)
+	mockService.EXPECT().
+		GetUser(gomock.Any()).
+		Return("Get user 101")
+	router := setupTestRouter(mockService)
 
 	id := "101"
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/users/%s", id), nil)
@@ -41,24 +53,36 @@ func Test_getUserById(t *testing.T) {
 
 	router.ServeHTTP(w, req)
 
-	expected := fmt.Sprintf(`{"message": "get user %s"}`, id)
+	expected := fmt.Sprintf(`{"message": "Get user %s"}`, id)
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.JSONEq(t, expected, w.Body.String())
 }
 
 func Test_createUser(t *testing.T) {
-	router := setupTestRouter()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockService := NewMockUserService(ctrl)
+	mockService.EXPECT().
+		CreateUser().Return("Create new user")
+
+	router := setupTestRouter(mockService)
 	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
 	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusCreated, w.Code)
-	assert.Contains(t, w.Body.String(), "create new user")
+	assert.Contains(t, w.Body.String(), "Create new user")
 }
 
 func Test_UpdateUserById(t *testing.T) {
-	router := setupTestRouter()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockService := NewMockUserService(ctrl)
+	mockService.EXPECT().
+		UpdateUser(gomock.Any()).Return("Update user 101")
+
+	router := setupTestRouter(mockService)
 	id := "101"
 	req, _ := http.NewRequest(http.MethodPatch, fmt.Sprintf("/users/%s", id), nil)
 	w := httptest.NewRecorder()
@@ -66,19 +90,24 @@ func Test_UpdateUserById(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Contains(t, w.Body.String(), "update user")
+	assert.Contains(t, w.Body.String(), "Update user")
 	assert.Contains(t, w.Body.String(), id)
 }
 
 func Test_deleteUserById(t *testing.T) {
-	router := setupTestRouter()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockService := NewMockUserService(ctrl)
+	mockService.EXPECT().
+		DeleteUser(gomock.Any()).Return("Delete user 101")
+	router := setupTestRouter(mockService)
 	id := "101"
 	req, _ := http.NewRequest(http.MethodDelete, fmt.Sprintf("/users/%s", id), nil)
 	w := httptest.NewRecorder()
 
 	router.ServeHTTP(w, req)
 
-	expected := fmt.Sprintf(`{"message": "delete user %s"}`, id)
+	expected := `{"message": "Delete user 101"}`
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.JSONEq(t, expected, w.Body.String())
 
--- internal/user/service_test.go
+++ internal/user/service_test.go
@@ -1,1 +1,40 @@
 package user
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_GetAllUsers(t *testing.T) {
+	want := "Get All users"
+	got := NewSimpleService().GetAllUsers()
+	assert.Equal(t, want, got)
+}
+
+func Test_GetUser(t *testing.T) {
+	id := "101"
+	want := "Get user 101"
+
+	got := NewSimpleService().GetUser(id)
+	assert.Equal(t, want, got)
+}
+
+func Test_CreateUser(t *testing.T) {
+	want := "Create new user"
+	got := NewSimpleService().CreateUser()
+	assert.Equal(t, want, got)
+}
+
+func Test_UpdateUser(t *testing.T) {
+	id := "101"
+	want := "Update user 101"
+	got := NewSimpleService().UpdateUser(id)
+	assert.Equal(t, want, got)
+}
+
+func Test_DeleteUser(t *testing.T) {
+	id := "101"
+	want := "Delete user 101"
+	got := NewSimpleService().DeleteUser(id)
+	assert.Equal(t, want, got)
+}
