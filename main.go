3945b8530d027a13e72dc1208642e65c7d042120
PandaHawk <mchlbng@gmail.com>
2025-07-16 14:50:22 +0200

    fix db bug
    Signed-off-by: PandaHawk <mchlbng@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/pandahawk/blog-api/internal/database"
 	"github.com/pandahawk/blog-api/internal/user"
 	"github.com/pandahawk/blog-api/router"
+	"gorm.io/gorm"
 	"log"
 )
 
@@ -16,10 +18,14 @@ func main() {
 	if err := db.AutoMigrate(&user.User{}); err != nil {
 		log.Fatal("failed to create db tables")
 	}
-	if err := db.Exec("ALTER SEQUENCE users_id_seq RESTART WITH 101;").
-		Error; err != nil {
-		log.Fatal("failed to alter sequence:", err)
+
+	if err := db.First(&user.User{}).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+		if err := db.Exec("ALTER SEQUENCE users_id_seq RESTART WITH 101;").
+			Error; err != nil {
+			log.Fatal("failed to alter sequence:", err)
+		}
 	}
+
 	r := gin.Default()
 	router.SetupRoutes(r, db)
 	if err := r.Run(":8080"); err != nil {
