f6916abc7b5515c6c82b45a860808266fca96c62
PandaHawk <mchlbng@gmail.com>
2025-07-16 19:52:41 +0200

    test repository
    Signed-off-by: PandaHawk <mchlbng@gmail.com>

--- internal/user/repository_test.go
+++ internal/user/repository_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func setupTestDB(t *testing.T) *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
 	require.NoError(t, err)
 	require.NoError(t, db.AutoMigrate(&User{}))
 	sampleUsers := []User{
@@ -26,9 +26,9 @@ func TestGormRepository_FindAll(t *testing.T) {
 	repo := NewGormRepository(db)
 
 	users, err := repo.FindAll()
+
 	require.NoError(t, err)
 	require.Len(t, users, 3)
-
 	assert.Equal(t, "testuser1", users[0].Username)
 	assert.Equal(t, "t1@example.com", users[0].Email)
 
@@ -39,7 +39,70 @@ func TestGormRepository_FindByID(t *testing.T) {
 	db := setupTestDB(t)
 	repo := NewGormRepository(db)
 	id := 1
+
 	got, err := repo.FindByID(id)
+
 	require.NoError(t, err)
 	assert.Equal(t, id, got.ID)
+}
+
+func TestGormRepository_FindByUsername(t *testing.T) {
+	db := setupTestDB(t)
+	repo := NewGormRepository(db)
+	username := "testuser1"
+
+	got, err := repo.FindByUsername(username)
+
+	require.NoError(t, err)
+	assert.Equal(t, username, got.Username)
+}
+
+func TestGormRepository_FindByEmail(t *testing.T) {
+	db := setupTestDB(t)
+	repo := NewGormRepository(db)
+	email := "t1@example.com"
+
+	got, err := repo.FindByEmail(email)
+
+	require.NoError(t, err)
+	assert.Equal(t, email, got.Email)
+}
+
+func TestGormRepository_Create(t *testing.T) {
+	db := setupTestDB(t)
+	repo := NewGormRepository(db)
+	user := User{Username: "newuser", Email: "newemail@mail.com"}
+
+	saved, err := repo.Create(user)
+
+	require.NoError(t, err)
+	assert.Equal(t, user.Username, saved.Username)
+
+	saved, err = repo.Create(user)
+	assert.Error(t, err)
+}
+
+func TestGormRepository_Update(t *testing.T) {
+	db := setupTestDB(t)
+	repo := NewGormRepository(db)
+	user := User{Username: "newuser", Email: "newemail@mail.com"}
+
+	updated, err := repo.Update(user)
+
+	require.NoError(t, err)
+	assert.Equal(t, user.Username, updated.Username)
+}
+
+func TestGormRepository_Delete(t *testing.T) {
+	db := setupTestDB(t)
+	repo := NewGormRepository(db)
+	user := User{
+		ID:       1,
+		Username: "testuser1",
+		Email:    "t1@example.com",
+	}
+
+	err := repo.Delete(user)
+
+	assert.NoError(t, err)
 }
