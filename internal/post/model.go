0af0af75ede8e1dbbef766db7e2bd49c48c5da81
Michael Obeng <michael.obeng@mail.schwarz>
2025-07-22 17:07:03 +0200

    update examples for swagger

--- internal/post/model.go
+++ internal/post/model.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Post struct {
-	ID        uuid.UUID `gorm:"type:char(36);primaryKey"` // @swagger:strfmt uuid
-	Title     string    `json:"title" gorm:"not null" binding:"required"`
-	Content   string    `json:"content" gorm:"type:text;not null" binding:"required"`
-	CreatedAt time.Time `json:"created_at" gorm:"not null"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
-	UserID    uuid.UUID `gorm:"type:char(36);not null;constraint:OnDelete:CASCADE;"`
+	ID        uuid.UUID `gorm:"type:char(36);primaryKey" example:"4e76b320-d5b7-4a0a-bb0f-2049fe6a91a7"`
+	Title     string    `json:"title" gorm:"not null" binding:"required" example:"My First Post"`
+	Content   string    `json:"content" gorm:"type:text;not null" binding:"required" example:"My First Post Content"`
+	CreatedAt time.Time `json:"created_at" gorm:"not null" example:"2025-07-18T15:04:05Z"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"not null" example:"2025-08-19T15:04:05Z"`
+	UserID    uuid.UUID `gorm:"type:char(36);not null;constraint:OnDelete:CASCADE;" example:"5e76b320-d5b7-4a0a-bb0f-2049fe3a91a4"`
 }
 
 //goland:noinspection GoExportedElementShouldHaveComment
