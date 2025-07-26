abdeb80282654e604beb2843bc374dc829e24116
PandaHawk <mchlbng@gmail.com>
2025-07-26 09:14:29 +0200

    start refactoring tests to table driven tests
    Signed-off-by: PandaHawk <mchlbng@gmail.com>

--- internal/shared/model/post.go
+++ internal/shared/model/post.go
@@ -8,13 +8,12 @@ import (
 
 type Post struct {
 	ID        uuid.UUID `gorm:"type:char(36);primaryKey" example:"4e76b320-d5b7-4a0a-bb0f-2049fe6a91a7"`
-	Title     string    `json:"title" gorm:"not null" binding:"required" example:"My First Post"`
-	Content   string    `json:"content" gorm:"type:text;not null" binding:"required" example:"My First Post Content"`
-	CreatedAt time.Time `json:"created_at" gorm:"not null" example:"2025-07-18T15:04:05Z"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"not null" example:"2025-08-19T15:04:05Z"`
-
-	UserID uuid.UUID `gorm:"type:char(36);not null;constraint:OnDelete:CASCADE;"  example:"5e76b320-d5b7-4a0a-bb0f-2049fe3a91a4"`
-	User   *User     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Title     string    `gorm:"not null" binding:"required" example:"My First Post"`
+	Content   string    `gorm:"type:text;not null" binding:"required" example:"My First Post Content"`
+	CreatedAt time.Time `gorm:"not null" example:"2025-07-18T15:04:05Z"`
+	UpdatedAt time.Time `gorm:"not null" example:"2025-08-19T15:04:05Z"`
+	UserID    uuid.UUID
+	User      *User `gorm:"constraint:OnDelete:CASCADE"`
 }
 
 func NewPost(title string, content string, authorID uuid.UUID) *Post {
