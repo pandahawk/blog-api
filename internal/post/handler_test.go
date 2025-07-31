dd2f9e056c55fa08a86f39988537c96c59e5cfa0
PandaHawk <mchlbng@gmail.com>
2025-07-31 11:39:21 +0200

    wip: start impl post handler test
    Signed-off-by: PandaHawk <mchlbng@gmail.com>

--- internal/post/handler_test.go
+++ internal/post/handler_test.go
@@ -1,9 +1,17 @@
 package post
 
 import (
+	"encoding/json"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/pandahawk/blog-api/internal/apperrors"
 	"github.com/pandahawk/blog-api/internal/shared/model"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -23,18 +31,343 @@ func setupTestRouterWithMockService(t *testing.T) (*gin.Engine, *MockService) {
 func TestHandler_GetPosts(t *testing.T) {
 	tests := []struct {
 		name          string
-		wantPost      *model.Post
-		want          *Response
-		mockbehaviour func(service *MockService, post *model.Post)
+		wantPosts     []*model.Post
+		want          []*Response
+		mockbehaviour func(service *MockService, posts []*model.Post)
+		wantStatus    int
+		wantErr       string
 	}{
-		{},
+		{
+			name: "success",
+			wantPosts: []*model.Post{
+				{
+					ID:      uuid.New(),
+					Title:   "title1",
+					Content: "content1",
+					User: &model.User{
+						ID:       uuid.New(),
+						Username: "user1",
+						Email:    "user1@mail.com"},
+				},
+				{
+					ID:      uuid.New(),
+					Title:   "title2",
+					Content: "content2",
+					User: &model.User{
+						ID:       uuid.New(),
+						Username: "user2",
+						Email:    "user2@mail.com"}},
+			},
+			want: []*Response{
+				{
+					Title:   "title1",
+					Content: "content1",
+					Author: UserSummaryResponse{
+						UserID:   uuid.Nil,
+						Username: "user1",
+					},
+				},
+				{
+					Title:   "title2",
+					Content: "content2",
+					Author: UserSummaryResponse{
+						UserID:   uuid.Nil,
+						Username: "user2",
+					},
+				},
+			},
+			mockbehaviour: func(service *MockService, posts []*model.Post) {
+				service.EXPECT().GetPosts().Return(posts, nil)
+			},
+			wantStatus: 200,
+			wantErr:    "",
+		},
+		{
+			name:      "failed",
+			wantPosts: nil,
+			want:      nil,
+			mockbehaviour: func(service *MockService, posts []*model.Post) {
+				service.EXPECT().GetPosts().Return(nil, errors.New("failed to get posts"))
+			},
+			wantStatus: 500,
+			wantErr:    "failed to get posts",
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			//router, mockService := setupTestRouterWithMockService(t)
-			//if test.mockbehaviour != nil {
-			//	test.mockbehaviour(mockService, test.wantPost)
-			//}
+			router, mockService := setupTestRouterWithMockService(t)
+			if test.mockbehaviour != nil {
+				test.mockbehaviour(mockService, test.wantPosts)
+
+				w := httptest.NewRecorder()
+				req, _ := http.NewRequest(http.MethodGet, "/posts", nil)
+				router.ServeHTTP(w, req)
+
+				assert.Equal(t, w.Code, test.wantStatus)
+				if test.wantErr == "" {
+					var r []*Response
+					if err := json.NewDecoder(w.Body).Decode(&r); err != nil {
+						t.Fatal(err)
+					}
+					for i, response := range r {
+						assert.Equal(t, response, r[i])
+						assert.Equal(t, test.wantPosts[i].Title, response.Title)
+						assert.Equal(t, test.wantPosts[i].User.Username, response.Author.Username)
+						assert.Equal(t, test.wantPosts[i].Content, response.Content)
+					}
+				} else {
+					assert.Contains(t, w.Body.String(), test.wantErr)
+				}
+			}
+		})
+	}
+}
+
+func TestHandler_GetPost(t *testing.T) {
+	tests := []struct {
+		name          string
+		id            string
+		wantPost      *model.Post
+		mockbehaviour func(service *MockService, id string, post *model.Post)
+		wantStatus    int
+		wantErr       string
+	}{
+		{
+			name: "success",
+			id:   uuid.Nil.String(),
+			wantPost: &model.Post{
+				ID:      uuid.Nil,
+				Title:   "title",
+				Content: "content",
+				UserID:  uuid.New(),
+				User: &model.User{
+					ID:       uuid.New(),
+					Username: "user1",
+					Email:    "user1@mail.com",
+				},
+			},
+			mockbehaviour: func(service *MockService, id string, post *model.Post) {
+				service.EXPECT().GetPost(gomock.Any()).Return(post, nil)
+			},
+			wantStatus: 200,
+			wantErr:    "",
+		},
+		{
+			name:     "not found",
+			id:       uuid.Nil.String(),
+			wantPost: nil,
+			mockbehaviour: func(service *MockService, id string, post *model.Post) {
+				service.EXPECT().GetPost(gomock.Any()).
+					Return(nil, apperrors.NewNotFoundError("post", uuid.Nil))
+			},
+			wantStatus: 404,
+			wantErr:    "not found",
+		},
+		{
+			name: "not an uuid",
+			id:   "abc",
+			wantPost: &model.Post{
+				ID:      uuid.Nil,
+				Title:   "title",
+				Content: "content",
+				UserID:  uuid.Nil,
+				User:    nil,
+			},
+			mockbehaviour: nil,
+			wantStatus:    400,
+			wantErr:       "ID must be a uuid",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			router, mockService := setupTestRouterWithMockService(t)
+			if test.mockbehaviour != nil {
+				test.mockbehaviour(mockService, test.id, test.wantPost)
+			}
+
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/posts/"+test.id, nil)
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, w.Code, test.wantStatus)
+			if test.wantErr == "" {
+
+			} else {
+				assert.Contains(t, w.Body.String(), test.wantErr)
+			}
+
+		})
+	}
+}
+
+func TestHandler_CreateUser(t *testing.T) {
+	tests := []struct {
+		name          string
+		rawBody       string
+		wantPost      *model.Post
+		mockBehaviour func(service *MockService, post *model.Post)
+		wantStatus    int
+		wantErr       string
+	}{
+		{
+			name: "success",
+			rawBody: `
+				{"title":"title",
+				"content":"content",
+				"author_id":"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"}
+				`,
+			wantPost: &model.Post{
+				ID:      uuid.MustParse("3c9a5f8d-91c6-4e3e-9f76-046b7e9b6c1a"),
+				Title:   "title",
+				Content: "content",
+				UserID:  uuid.MustParse("3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"),
+				User: &model.User{ID: uuid.MustParse(
+					"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a")},
+			},
+			mockBehaviour: func(service *MockService, post *model.Post) {
+				service.EXPECT().CreatePost(gomock.Any()).Return(post, nil)
+			},
+			wantStatus: 201,
+			wantErr:    "",
+		},
+		{
+			name: "invalid json body",
+			rawBody: `
+				{"title":"title",
+				"content":"content",
+				"author_id":"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"
+				`,
+			wantPost:      nil,
+			mockBehaviour: nil,
+			wantStatus:    400,
+			wantErr:       "invalid json",
+		},
+		{
+			name: "invalid title",
+			rawBody: `
+				{"title":"123",
+				"content":"content",
+				"author_id":"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"}
+				`,
+			wantPost: nil,
+			mockBehaviour: func(service *MockService, post *model.Post) {
+				service.EXPECT().CreatePost(gomock.Any()).
+					Return(nil, apperrors.NewInvalidInputError(
+						"invalid title"))
+			},
+			wantStatus: 400,
+			wantErr:    "invalid",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			router, mockService := setupTestRouterWithMockService(t)
+			if test.mockBehaviour != nil {
+				test.mockBehaviour(mockService, test.wantPost)
+			}
+
+			w := httptest.NewRecorder()
+			body := strings.NewReader(test.rawBody)
+			req, _ := http.NewRequest(http.MethodPost, "/posts", body)
+			req.Header.Set("Content-Type", "application/json")
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, test.wantStatus, w.Code)
+			if test.wantErr == "" {
+				var r *Response
+				if err := json.NewDecoder(w.Body).Decode(&r); err != nil {
+					t.Fatal(err)
+				}
+				assert.Equal(t, test.wantPost.Title, r.Title)
+				assert.Equal(t, test.wantPost.Content, r.Content)
+			}
+		})
+	}
+}
+
+func TestHandler_UpdateUser(t *testing.T) {
+	tests := []struct {
+		name          string
+		rawBody       string
+		id            string
+		wantPost      *model.Post
+		mockBehaviour func(service *MockService, id uuid.UUID, post *model.Post)
+		wantStatus    int
+		wantErr       string
+	}{
+		{
+			name: "success",
+			rawBody: `
+				{"title":"updated",
+				"content":"content updated",
+				"author_id":"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"}
+				`,
+			id: "3c9a5f8d-91c6-4e3e-9f76-046b7e9b6c1a",
+			wantPost: &model.Post{
+				ID:      uuid.MustParse("3c9a5f8d-91c6-4e3e-9f76-046b7e9b6c1a"),
+				Title:   "updated",
+				Content: "content updated",
+				UserID:  uuid.MustParse("3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"),
+				User: &model.User{ID: uuid.MustParse(
+					"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a")},
+			},
+			mockBehaviour: func(service *MockService, id uuid.UUID, post *model.Post) {
+				service.EXPECT().CreatePost(gomock.Any()).Return(post, nil)
+			},
+			wantStatus: 200,
+			wantErr:    "",
+		},
+		//{
+		//	name: "invalid json body",
+		//	rawBody: `
+		//		{"title":"title",
+		//		"content":"content",
+		//		"author_id":"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"
+		//		`,
+		//	wantPost:      nil,
+		//	mockBehaviour: nil,
+		//	wantStatus:    400,
+		//	wantErr:       "invalid json",
+		//},
+		//{
+		//	name: "invalid title",
+		//	rawBody: `
+		//		{"title":"123",
+		//		"content":"content",
+		//		"author_id":"3c9d5f8d-91c6-4e3e-9f76-046b7e9b6c1a"}
+		//		`,
+		//	wantPost: nil,
+		//	mockBehaviour: func(service *MockService, post *model.Post) {
+		//		service.EXPECT().CreatePost(gomock.Any()).
+		//			Return(nil, apperrors.NewInvalidInputError(
+		//				"invalid title"))
+		//	},
+		//	wantStatus: 400,
+		//	wantErr:    "invalid",
+		//},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			router, mockService := setupTestRouterWithMockService(t)
+			if test.mockBehaviour != nil {
+				test.mockBehaviour(mockService, uuid.MustParse(test.id), test.wantPost)
+			}
+
+			w := httptest.NewRecorder()
+			body := strings.NewReader(test.rawBody)
+			req, _ := http.NewRequest(http.MethodPatch, "/posts/"+test.id, body)
+			req.Header.Set("Content-Type", "application/json")
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, test.wantStatus, w.Code)
+			if test.wantErr == "" {
+				var r Response
+				if err := json.NewDecoder(w.Body).Decode(&r); err != nil {
+					t.Fatal(err)
+				}
+				assert.Equal(t, test.wantPost.Title, r.Title)
+				assert.Equal(t, test.wantPost.Content, r.Content)
+			}
 		})
 	}
 }
